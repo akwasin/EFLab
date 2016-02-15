f501dd0247cd2bf8e7427322d309a0913ae96e02
akwasin <akwasiijunior@gmail.com>
2016-02-15 15:20:35 +0100

    new file structure + lap 3 with linq

---
+++ EFLab_3_LINQ/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFLab_3_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> nameList = new List<string>();
+            nameList.Add("AJ");
+            nameList.Add("Sandra");
+            nameList.Add("Theodore");
+            nameList.Add("Akwasi");
+            nameList.Add("Thommy");
+            nameList.Add("AkwasiiJunior");
+            nameList.Add("Christina");
+            nameList.Add("Millie");
+            nameList.Add("Akwasi Junior");
+            nameList.Add("Pappa");
+            nameList.Add("Kofi");
+            nameList.Add("A. Junior");
+
+
+
+            Console.WriteLine("1 - View all names");
+            Console.WriteLine("2 - View all names that beings with A");
+            string userChoice = Console.ReadLine();
+
+            
+
+            if (userChoice == "1")
+            {
+             Console.WriteLine($"You Selected 1 \r\n");
+                foreach (string name in nameList)
+                {
+                    Console.WriteLine(name);
+                }
+            }
+            else if (userChoice == "2")
+            {
+             Console.WriteLine("You Selected 2");
+                foreach (string name in nameList)
+                {
+                    if (name.StartsWith("A"))
+                    {
+                        Console.WriteLine(name);
+                    }
+                }
+            }
+
+            System.Threading.Thread.Sleep(9000);
+        }
+    }
+}
