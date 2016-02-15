994399b3bb96cba53545160312c0aa138bb4db53
akwasin <akwasiijunior@gmail.com>
2016-02-15 13:39:07 +0100

    Added lab 1 answers

---
+++ EFLab_1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFLab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        /*
+
+        1. Entity framework är ett ado.net ramverk som förenklar arbetet av relationsdata. Med hjälp av Entity framwork kan utvecklare skapa frågor med LINQ som hämta och manipulerar data.
+
+        2/3. Model to database (Utvecklaren bygger en modell som generar DB - kommer att gå ur tiden, ingen     support av   MS)
+        Code First (DB med tabeller och kolumner genereras utifrån skapade c# klasser som har skapats i Visual  Studio)
+        Database First (Modellen genereras utifrån skapad databas, dvs jobbet börar i SQL Management Studio)
+        
+        4. Create, Read, Update, Delete. Som orden säger, skapa, läsa, uppdatera och ta bort.
+        
+        5. O/RM står för Object / Relational Mapping och är en typ av ramverk där Entity framwork hamnar under.     Det är   en   förättring av ADO.NET som automatiserar mekanismen för åtkomst/lagring av data i en   databas.
+        
+        6. Min skjorta är sommar Rosa
+        
+        7. Klassen länkar samma de olika underklasserna / ger entitet till de andra klasserna
+        
+        8. 
+        a. Den får namnet xx.contex.tt. Ett auto genererat namn.
+        b. Den heter xx.tt. Även detta är ett autogenererat namn.
+        c. I app.config ligger bland annat vår "connectionstring" som kopplar upp oss mot databasen med hjälp av          användarnamn och lösenord.
+        
+        9. LINQ och Entity SQL. Den första av dessa är ett sätt att skriva frågor (querys) mot objektmodellen.  Frågan     returnerar entiteter som defineras i modellen. Den andra är likt LINQ ett sätt att ställa     frågor mot en DB.
+            */
+        }
+    }
+}
