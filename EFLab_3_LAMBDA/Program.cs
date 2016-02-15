e424a81d9d651995889b27dc0e34a245bac3a8da
akwasin <akwasiijunior@gmail.com>
2016-02-15 15:44:40 +0100

    lab 3 med linq

---
+++ EFLab_3_LAMBDA/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFLab_3_LAMBDA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+
+/*
+3.4.1		Linq	med	Lambda	(mot	en	enkel	lista)	
+Fortsätt	med	konsol	applikationen	från	2.3
+Gör	om Linq-frågorna	så	att	de	använder	Linq	med	Lambda
+Skapa	alternativ som	användaren	kan välja	och	implementera	logiken för	följande	
+alternativ
+o 3.	Visa	alla	namn	som	innehåller	ett	“A”
+o 4.	Visa	alla	namn	som	börjar	på	“A”	och	inte	innehåller	ett	“S”
+    persons.Where(p => p.FirstName.StartsWith("A"))
+*/
