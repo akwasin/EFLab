92909c1b919fa84bbf922b4f2594a80f0f9d0b0b
akwasin <akwasiijunior@gmail.com>
2016-02-24 11:21:21 +0100

    begin crudding

---
+++ EFLab_4_CRUD/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFLab_4_CRUD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            /*
+            Skapa	en	ny	Konsol	Applikation
+                • Gör	en	DB	First	modell	av	Books-databasen
+                • Du	ska	använda	Separation	of	concerns	I	applkationen
+                • Din	konsol	App	ska	kunna	göra	följande
+                o Visa	en	lista	på	alla	Författare
+                o Kunna	hitta	en	författare	som	startar	på	en	viss	sträng.	Den	ska	söka	både	I	
+                förnamn	och	efternamn.
+                o Hitta	en	författare	baserat	på	ID
+                o Lägga	till en	ny	författare.
+                o Uppdatera	en	författares	Namn
+                § Hitta	en	författare	genom	att	skriva	in	bade	förnamn	och	efternamn.
+                § Om	fler	en	en	författare	hittas	ge	då	ett	meddelande	om	detta.
+                § När	författaren	hittats	be	om	både	för	och efternamn	och	uppdatera	
+                detta	i Databasen.
+                o Ta	bort	en	författare (Hur	vill	du	kunna	hitta	honom??)
+                o Uppdatera	en	författares	ålder
+                • Din	app	ska	innehålla	felhantering /statusmeddelanden.
+                o Dvs	om	t	ex	något	inte	hittas	ska	det	komma	ett	meddelande	om	detta
+                o Programmet	ska	inte	krascha	om	man	matar	in	t	ex	ett	id	som	inte	finns.
+            */
+        }
+    }
+}
